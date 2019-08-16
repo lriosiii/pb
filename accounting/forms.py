0b9cacd70f8f72e2e07a710780332fd0ab131215
Luis Rios <15820888+lriosiii@users.noreply.github.com>
2019-08-16 10:39:35 -0400

    misc

--- accounting/forms.py
+++ accounting/forms.py
@@ -1,12 +1,8 @@
 from django import forms
 
 
-class NBForm(forms.Form):
-    file = forms.FileField(allow_empty_file=False)
-
-
-class RevenueForm(forms.Form):
-    file = forms.FileField(allow_empty_file=False)
-
+class NBRevenueForm(forms.Form):
+    nb_form = forms.FileField(allow_empty_file=False)
+    revenue_form = forms.FileField(allow_empty_file=False)
 
 
--- accounting/views.py
+++ accounting/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from .forms import NBForm, RevenueForm
+from .forms import NBRevenueForm
+import csv
+import openpyxl
 
 
 class AccountingHome(TemplateView):
@@ -8,6 +10,14 @@ class AccountingHome(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['NBForm'] = NBForm
-        context['RevenueForm'] = RevenueForm
+        context['NBRevenueForm'] = NBRevenueForm
         return context
+
+    def post(self, request, *args, **kwargs):
+        nb_input_file = request.FILES['nb_form']
+        nb_wb = openpyxl.load_workbook(nb_input_file)
+        nb_worksheet = nb_wb['JulyRev']  # TODO month
+
+        revenue_file_input = request.FILES['revenue_form']
+        revenue_wb = openpyxl.load_workbook(revenue_file_input, read_only=True)
+        revenue_worksheet = revenue_wb['Reconciliation Report']
