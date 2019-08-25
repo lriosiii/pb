d36163b4f16b151359b0c2dc85e24b8ef8e957a0
Luis <Luis@Ashleys-Air.attlocal.net>
2019-08-25 13:38:04 -0400

    formatting

--- accounting/forms.py
+++ accounting/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class CompareNbRevenueForm(forms.Form):
-    nb_file = forms.FileField(allow_empty_file=False)
-    reconciliation_file = forms.FileField(allow_empty_file=False)
+    nb_file = forms.FileField(allow_empty_file=False, label="New Biz")
+    reconciliation_file = forms.FileField(allow_empty_file=False, label="Reconciliation")
 
 
