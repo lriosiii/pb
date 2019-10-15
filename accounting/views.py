440e89fe861015cdb5ac362705f9bd014a6f79fd
Luis.Rios <lrios@charterschoolsusa.com>
2019-10-15 00:44:45 -0400

    wip - push for tuesday partial credit

--- accounting/views.py
+++ accounting/views.py
@@ -19,7 +19,7 @@ class AccountingHome(TemplateView):
 
     def post(self, request, *args, **kwargs):
         filename = 'myexport'
-        from openpyxl.styles import Font
+        from openpyxl.styles import Font, PatternFill
         from openpyxl.styles.colors import RED
         try:
             revenue_file_input = request.FILES['reconciliation_file']
@@ -67,13 +67,22 @@ class AccountingHome(TemplateView):
                         cell.font = Font(color=RED)
 
         if request.POST.get('cbar'):
-            filename = 'AGEING CB - PR {mm-dd-yy}'
+            filename = 'AGEING CB - PR'
             cb_wb = openpyxl.load_workbook(request.FILES['CB_file'], read_only=True)
-            ar_wb = openpyxl.load_workbook(request.FILES['AR_file'])
+            resp_wb = openpyxl.load_workbook(request.FILES['AR_file'])
             cb_worksheet = cb_wb.active
-            ar_worksheet = ar_wb.active
+            ws = resp_wb.active
+            cb_filteredrows = (row for row in cb_worksheet.iter_rows(min_row=3) if row[0].value is not None and int(row[0].value) >= 45)
+            ws_populatedrows = (row for row in ws.iter_rows(min_row=3) if row[0].value is not None)
+            cb_orngyellow_invoices = list((row[5].value for row in cb_filteredrows if row[0].fill.start_color.index in ('FFFFFF00', 'FFFFC000')))
+            for  row in ws_populatedrows:
+                if int(row[0].value) < 45:
+                    ws.row_dimensions[row[0].row].hidden = True
+                if row[5].value in cb_orngyellow_invoices:
+                    for cell in row:
+                        cell.fill = PatternFill("solid", fgColor="FFFFFF00")
 
         response = HttpResponse(content=save_virtual_workbook(resp_wb), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = f'attachment; filename={filename}.xlsx'
+        response['Content-Disposition'] = f'attachment; filename=exportpb.xlsx'
         return response
 
