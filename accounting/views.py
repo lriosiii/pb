012099351997b7a549e551405d0c91a8a0c69676
Luis.Rios <lrios@charterschoolsusa.com>
2019-10-16 00:36:36 -0400

    finish

--- accounting/views.py
+++ accounting/views.py
@@ -20,7 +20,7 @@ class AccountingHome(TemplateView):
     def post(self, request, *args, **kwargs):
         filename = 'myexport'
         from openpyxl.styles import Font, PatternFill
-        from openpyxl.styles.colors import RED
+        from openpyxl.styles.colors import RED, BLUE
         try:
             revenue_file_input = request.FILES['reconciliation_file']
             revenue_wb = openpyxl.load_workbook(revenue_file_input, read_only=True, data_only=True)
@@ -67,22 +67,36 @@ class AccountingHome(TemplateView):
                         cell.font = Font(color=RED)
 
         if request.POST.get('cbar'):
-            filename = 'AGEING CB - PR'
+            filename, date = 'AGEING-CB-PR', datetime.datetime.now().date()
             cb_wb = openpyxl.load_workbook(request.FILES['CB_file'], read_only=True)
             resp_wb = openpyxl.load_workbook(request.FILES['AR_file'])
             cb_worksheet = cb_wb.active
             ws = resp_wb.active
+            cb_orngyellow_invoices, cb_orngfill, cb_bluetext = list(), list(), list()
             cb_filteredrows = (row for row in cb_worksheet.iter_rows(min_row=3) if row[0].value is not None and int(row[0].value) >= 45)
+            for row in cb_filteredrows:
+                if row[0].fill.start_color.index in ('FFFFFF00', 'FFFFC000'):
+                    cb_orngyellow_invoices.append(row[5].value)
+                elif row[0].value >= 65 and row[0].font.color.value in ('FF00B0F0'):
+                    cb_orngfill.append(row[5].value)
+                elif row[0].value < 65 and row[0].font.color.value in ('FF00B0F0'):
+                    cb_bluetext.append(row[5].value)
             ws_populatedrows = (row for row in ws.iter_rows(min_row=3) if row[0].value is not None)
-            cb_orngyellow_invoices = list((row[5].value for row in cb_filteredrows if row[0].fill.start_color.index in ('FFFFFF00', 'FFFFC000')))
-            for  row in ws_populatedrows:
+            for row in ws_populatedrows:
                 if int(row[0].value) < 45:
                     ws.row_dimensions[row[0].row].hidden = True
                 if row[5].value in cb_orngyellow_invoices:
                     for cell in row:
                         cell.fill = PatternFill("solid", fgColor="FFFFFF00")
+                if row[5].value in cb_orngfill:
+                    # to orange
+                    for cell in row:
+                        cell.fill = PatternFill("solid", fgColor="FFFFC000")
+                if row[5].value in cb_bluetext:
+                    for cell in row:
+                        cell.font = Font(color=BLUE)
 
         response = HttpResponse(content=save_virtual_workbook(resp_wb), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = f'attachment; filename=exportpb.xlsx'
+        response['Content-Disposition'] = f'attachment; filename={filename}_{date}.xlsx'
         return response
 
